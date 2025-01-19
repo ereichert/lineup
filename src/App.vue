5c0092df9af106f6f6616d5f61833b3106ef3642
Eric Reichert <eric@reichertconsulting.com>
2025-01-19 14:54:45 -0500

    Removed most of the cruft leftover from the original vue template.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-// import { RouterLink, RouterView } from 'vue-router'
-// import HelloWorld from './components/HelloWorld.vue'
 import BattingLineup from './components/BattingLineup.vue'
 import FieldingLineup from './components/FieldingLineup.vue';
 
@@ -11,18 +9,6 @@ const NUM_FIELDING_POSITIONS = 9
 </script>
 
 <template>
-  <header>
-    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div> -->
-  </header>
 
   <body>
     <div class="container">
@@ -37,8 +23,6 @@ const NUM_FIELDING_POSITIONS = 9
       </div>
     </div>
   </body>
-
-  <!-- <RouterView /> -->
 </template>
 
 <style scoped>
@@ -56,66 +40,4 @@ body {
 .column {
   flex: 1;
 }
-
-/* header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-} */
 </style>
