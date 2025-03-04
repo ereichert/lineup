8ae7af01bbc944343e311d4b0204a080768c6a60
Eric Reichert <eric@reichertconsulting.com>
2025-03-04 12:04:55 -0500

    Configured the prettier to use 4 spaces for an indent. Got rid of the default tests from the project scaffolding. Added tests for the lineup validations that have to happen before a lineup is printed. Added navigation guards on the /print route using the lineup validators.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,33 @@
 import EditLineupsView from '@/views/EditLineupsView.vue'
+import luValidations from '@/validation/lineup-validators'
 import PrintView from '@/views/PrintView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import { useLineupsStore } from '@/stores/lineups'
+import { usePlayersStore } from '@/stores/players'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'edit lineups',
-      component: EditLineupsView
-    },
-    {
-      path: '/print',
-      name: 'print',
-      component: PrintView
-    }
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'edit lineups',
+            component: EditLineupsView
+        },
+        {
+            path: '/print',
+            name: 'print',
+            component: PrintView,
+            // TODO: When the validation fails errors should be shown on the edit lineup screen.
+            beforeEnter: (to, from) => {
+                const { battingLineup, fieldingLineup } = useLineupsStore()
+                const { players } = usePlayersStore()
+                return (
+                    luValidations.isValidBattingLineup(players, battingLineup) &&
+                    luValidations.isValidFieldingLineup(players, fieldingLineup)
+                )
+            }
+        }
+    ]
 })
 
 export default router
