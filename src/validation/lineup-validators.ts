e24df0c7cc1181083235afdba7842915caae57fd
Eric Reichert <eric@reichertconsulting.com>
2025-03-24 13:36:19 -0400

    Updated the formatting for the lineup-validators.ts to match the new tab config.

--- src/validation/lineup-validators.ts
+++ src/validation/lineup-validators.ts
@@ -1,43 +1,43 @@
 import type Player from '@/models/Player'
 
 const isValidBattingLineup = (
-    players: Array<Player>,
-    battingLineup: Record<string, string>
+  players: Array<Player>,
+  battingLineup: Record<string, string>
 ): boolean => {
-    const battingLineupIds = Object.values(battingLineup)
+  const battingLineupIds = Object.values(battingLineup)
 
-    if (battingLineupIds.length !== players.length) {
-        return false
-    }
+  if (battingLineupIds.length !== players.length) {
+    return false
+  }
 
-    return players.filter((player) => !battingLineupIds.includes(player.id)).length === 0
+  return players.filter((player) => !battingLineupIds.includes(player.id)).length === 0
 }
 
 const isValidFieldingLineup = (
-    players: Array<Player>,
-    fieldingLineup: Array<Map<string, string>>
+  players: Array<Player>,
+  fieldingLineup: Array<Map<string, string>>
 ): boolean => {
-    let isValid = true
-    for (let inning = 0; inning < fieldingLineup.length; inning++) {
-        const nextFieldingLineup = fieldingLineup[inning]
-        // This case probably means the correct number of bench players were not added to the initial lineup.
-        // If the correct number of bench players were not added to the initial lineup the lineup editing view should
-        // not show the correct number of bench positions.
-        if (nextFieldingLineup.size !== players.length) {
-            isValid = false
-            break
-        }
-
-        // Since we have validated that the lineup has the same number of available players
-        // the only way this case can happen is if one of the players has not been assigned a fielding position during
-        // one of the innings. This most likely means a player was selected twice also.
-        const fieldingLineupIds = Array.from(nextFieldingLineup.values())
-        if (players.filter((player) => !fieldingLineupIds.includes(player.id)).length !== 0) {
-            isValid = false
-            break
-        }
+  let isValid = true
+  for (let inning = 0; inning < fieldingLineup.length; inning++) {
+    const nextFieldingLineup = fieldingLineup[inning]
+    // This case probably means the correct number of bench players were not added to the initial lineup.
+    // If the correct number of bench players were not added to the initial lineup the lineup editing view should
+    // not show the correct number of bench positions.
+    if (nextFieldingLineup.size !== players.length) {
+      isValid = false
+      break
     }
-    return isValid
+
+    // Since we have validated that the lineup has the same number of available players
+    // the only way this case can happen is if one of the players has not been assigned a fielding position during
+    // one of the innings. This most likely means a player was selected twice also.
+    const fieldingLineupIds = Array.from(nextFieldingLineup.values())
+    if (players.filter((player) => !fieldingLineupIds.includes(player.id)).length !== 0) {
+      isValid = false
+      break
+    }
+  }
+  return isValid
 }
 
 export default { isValidBattingLineup, isValidFieldingLineup }
